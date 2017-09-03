088faf1cda37d93c276a51cbc3d9a5f92c650920
deebnntt <daniellebennettchan@gmail.com>
2017-09-03 17:13:27 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,14 +6,19 @@ attr_accessor :word
     @word = word
   end
 
+  #original code
+  # def match(array)
+  #   match_array = []
+  #   array.each do |i|
+  #     if i.split("").sort == @word.split("").sort
+  #       match_array << i
+  #     end
+  #   end
+  #   match_array
+  # end
+
   def match(array)
-    match_array = []
-    array.each do |i|
-      if i.split("").sort == @word.split("").sort
-        match_array << i
-      end
-    end
-    match_array
+    array.select {|i| i.split("").sort == @word.split("").sort }
   end
 
 end
